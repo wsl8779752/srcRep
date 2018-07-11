8c5ad577c6eea2152e5adddbc55231b1a52d1282
wei <rensheng946@163.com>
2018-07-11 18:19:42 +0800

    before go to danghsan

--- learnc++/chapter3/part4.cpp
+++ learnc++/chapter3/part4.cpp
@@ -8,9 +8,33 @@ using namespace std;
 class headQuators 
 { 
     public: 
-        headQuators (); 
+        headQuators(string name1){ name = name1;}; 
         virtual ~headQuators (); 
+        void setLifeElement(int n){
+             life_element = n ;
+        } 
+        void setManufacot_sequency:(){};
+        int manufactor_warrior(string warrior_name);
+    private: 
+        static int life_element; 
+        string manufactor_sequence[5];
+        string name;
+}; 
+class warrior 
+{ 
+    public: 
+        warrior (); 
+        virtual ~warrior (); 
          
     private: 
-        int 
+        int hp;
+        int number;
+        int damage;
 }; 
+int main(){
+    headQuators RedCommand("red"), Blue("blue");
+        
+
+
+
+}
