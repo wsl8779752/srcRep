9f731fc9fb80d628d0190d02162392276af744ae
weiHomePC <rensheng946@163.com>
2019-03-19 23:07:52 +0800

    悦得机器人正解可以实现输入测试

--- explosion/src/main.cpp
+++ explosion/src/main.cpp
@@ -4,11 +4,16 @@
 #include <kdl/frames_io.hpp> 
 int main(int argc, char *argv[])
 {
-    KDL::Chain *p = KDL::ExpYuede(); 
-    KDL::ChainFkSolverPos_recursive fksolver(*p)  ;
-    KDL::JntArray myinput(6);
-    KDL::Frame pos_goal;
-    fksolver.JntToCart(myinput,pos_goal,6);
+    KDL::Chain *p = KDL::ExpYuede(); //定义一个悦得机器人 
+    KDL::ChainFkSolverPos_recursive fksolver(*p);//定义一个机器人正动力学解对象
+    unsigned int nj = p->getNrOfJoints(); //获取机器人的关节数目
+    KDL::JntArray myinput(nj);  //定义一个关节空间量，长度是越的机器人的关节数
+    for (unsigned int i = 0; i < nj; ++i) { //从外部读入关节量
+        std::cin>>myinput.data(i); 
+    } 
+    KDL::Frame pos_goal; //定义一个se(3)量pos_goal
+    fksolver.JntToCart(myinput,pos_goal,6);//调用fksolver结算，结果存放到pos_goal
     std::cout<<"pos "<<pos_goal.p[0]<<"\n"<<pos_goal.p[1]<<"\n"<<pos_goal.p[2]<<
         "\n"<<"rotation "<<pos_goal.M<<std::endl;
+    delete p;
 }
