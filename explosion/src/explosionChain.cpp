7e1f85926273483a4ea0dcc3badb377747e12d79
wei <rensheng946@163.com>
2019-03-20 18:55:23 +0800

    测试puma560没通过,一键编译没通过

--- explosion/src/explosionChain.cpp
+++ explosion/src/explosionChain.cpp
@@ -7,14 +7,14 @@ namespace KDL{
         Chain *expYuede = new Chain;
         //设置关节参数，选项依次为旋转轴，减速比，直线结构的offset，电机惯量，
         //阻尼（滑动摩擦系数），刚度
-        expYuede->addSegment(Segment(Joint(Joint::RotZ,-62111,0,200e-6,1.48e-3,0), 
+        expYuede->addSegment(Segment(Joint(Joint::RotZ,1/-62.111,0,200e-6,1.48e-3,0), 
                     Frame::DH(0.0,M_PI_2,0.0,0.0), //a alpha  d theta;
                     //m,cog from origin,惯性张量擦书 
                     RigidBodyInertia(0,Vector::Zero(),RotationalInertia(0,0.35,0,0,0,0)))); 
 
         expYuede->addSegment(Segment(Joint(Joint::RotZ,107.815,0,200e-6,0.817e-3,0), 
                     Frame::DH(0.4318,0.0,0.0,0.0),
-                    RigidBodyInertia(17.4,Vector(-.3638,.006,.2275),RotationalInertia(0.13,0.524,0.539,0,0,0))));
+                    RigidBodyInertia(17.4,Vector(-0.3638,.006,.2275),RotationalInertia(0.13,0.524,0.539,0,0,0))));
 
         expYuede->addSegment(Segment(Joint(Joint::RotZ,-53.7063,0,200e-6,1.38e-3,0),
                     Frame::DH(0.0203,-M_PI_2,0.15005,0.0),
@@ -26,11 +26,11 @@ namespace KDL{
 
         expYuede->addSegment(Segment(Joint(Joint::RotZ,71.923,0,33e-6,82.6e-6,0),
                     Frame::DH(0.0,-M_PI_2,0.0,0.0),
-                    RigidBodyInertia(0.34,Vector::Zero(),RotationalInertia(.3e-3,.4e-3,.3e-3,0,0,0))));
+                    RigidBodyInertia(0.34,Vector::Zero(),RotationalInertia(0.3e-3,0.4e-3,0.3e-3,0,0,0))));
 
         expYuede->addSegment(Segment(Joint(Joint::RotZ,76.686,0,33e-6,36.7e-6,0),
                     Frame::DH(0.0,0.0,0.0,0.0),
-                    RigidBodyInertia(0.09,Vector(0,0,.032),RotationalInertia(.15e-3,0.15e-3,.04e-3,0,0,0))));
+                    RigidBodyInertia(0.09,Vector(0,0,.032),RotationalInertia(0.15e-3,0.15e-3,0.04e-3,0,0,0))));
 
         return expYuede;
     }
--- explosion/src/main.cpp
+++ explosion/src/main.cpp
@@ -1,18 +1,26 @@
 #include "explosionChain.hpp" 
 #include <iostream> 
-#include <kdl/chainfksolverpos_recursive.hpp> 
-#include <kdl/frames_io.hpp> 
+#include <chainfksolverpos_recursive.hpp> 
+#include <frames_io.hpp> 
+#include "models.hpp" 
 int main(int argc, char *argv[])
 {
-    KDL::Chain *p = KDL::ExpYuede(); //定义一个悦得机器人 
-    KDL::ChainFkSolverPos_recursive fksolver(*p);//定义一个机器人正动力学解对象
+    KDL::Chain *p = KDL::ExpYuede();; 
+    KDL::Chain chain;
+	chain = KDL::Puma560();
+    KDL::ChainFkSolverPos_recursive fksolver(*p)  ;
+    KDL::ChainFkSolverPos_recursive fksolverPuma560(chain)  ;
+    KDL::Frame pos_goal;
     unsigned int nj = p->getNrOfJoints(); //获取机器人的关节数目
-    KDL::JntArray myinput(nj);  //定义一个关节空间量，长度是越的机器人的关节数
+    KDL::JntArray myinput(nj);  //定义一个关节空间量，长度是机器人的关节数
     for (unsigned int i = 0; i < nj; ++i) { //从外部读入关节量
         std::cin>>myinput.data(i); 
     } 
-    KDL::Frame pos_goal; //定义一个se(3)量pos_goal
-    fksolver.JntToCart(myinput,pos_goal,6);//调用fksolver结算，结果存放到pos_goal
+    fksolver.JntToCart(myinput,pos_goal,nj);
+    std::cout<<"pos "<<pos_goal.p[0]<<"\n"<<pos_goal.p[1]<<"\n"<<pos_goal.p[2]<<
+        "\n"<<"rotation "<<pos_goal.M<<std::endl;
+     
+    fksolverPuma560.JntToCart(myinput,pos_goal,6);
     std::cout<<"pos "<<pos_goal.p[0]<<"\n"<<pos_goal.p[1]<<"\n"<<pos_goal.p[2]<<
         "\n"<<"rotation "<<pos_goal.M<<std::endl;
     delete p;
